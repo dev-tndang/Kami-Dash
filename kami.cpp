c6449fbf6778666721aac5fcda7d8542749e8b29
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-09-06 00:09:52 -0400

    Update 2023.09.06.01
    Added Delta Time into the while loop to be multiplied to the Gravity and Velocity at points needing to follow a more consistent time since the last frame

--- kami.cpp
+++ kami.cpp
@@ -22,15 +22,18 @@ int main()
     kamiPosition.x = windowWidth/2 - kamiRectangle.width/2;
     kamiPosition.y = windowHeight - kamiRectangle.height;
 
-    // Gravity Properties
-    const int gravity = 1;
-    const int jumpVelocity = -20;
+    // Gravity Properties (pixels/s)
+    const int gravity = 1600;
+    const int jumpVelocity = -800;
     int velocity = 0;
     int inTheAir = false;
 
     // The Main Game
     while (!WindowShouldClose())
     {
+        // Delta Time
+        const float deltaTime = GetFrameTime();
+
         BeginDrawing();
         ClearBackground(WHITE);
         
@@ -41,12 +44,13 @@ int main()
         {
             // Player is on the Ground
             velocity = 0;
+            kamiPosition.y = windowHeight - kamiRectangle.height;
             inTheAir = false;
         }
         else
         {
             // Player is in the Air, apply Gravity
-            velocity += gravity;
+            velocity += gravity * deltaTime;
             inTheAir = true;
         }
 
@@ -57,7 +61,7 @@ int main()
         }
         
         // Update Position
-        kamiPosition.y += velocity;
+        kamiPosition.y += velocity * deltaTime;
 
         EndDrawing();
     }
