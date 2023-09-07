74e97d6ebb91b2f9c74e5525ac3edf42bbcd6488
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-09-06 23:41:50 -0400

    Update 2023.09.06.03
    Added the Rectangle and Vector2 positioning to the Power Crystal power up, giving it a movement velocity to move to the left on the x-axis. Then moved the update animation into the ground check to only be updated when on the ground, freeze framing Kami when she is in the air

--- kami.cpp
+++ kami.cpp
@@ -24,11 +24,24 @@ int main()
 
     // Set PowerUp
     Texture2D powerCrystal = LoadTexture("textures/power_ups/crystals/blue/blue_crystal_sprites_sheet.png");
+    Rectangle powerCrystalRectangle;
+    Vector2 powerCrystalPosition;
+
+    powerCrystalRectangle.width = powerCrystal.width/4;
+    powerCrystalRectangle.height = powerCrystal.height;
+    powerCrystalRectangle.x = 0;
+    powerCrystalRectangle.y = 0;
+
+    powerCrystalPosition.x = windowWidth;
+    powerCrystalPosition.y = windowHeight - powerCrystalRectangle.height;
 
     // Kami's Running Animation
     const float updateTime = 1.0/12.0;
     float runningTime = 0;
     int kamiRunningFrame = 0;
+
+    // Power Crystal Movement
+    int powerCrystalVelocity = -600;
 
     // Gravity Properties (pixels/s)
     const int gravity = 1600;
@@ -46,7 +59,11 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
         
+        // Render Kami
         DrawTextureRec(kami, kamiRectangle, kamiPosition, WHITE);
+
+        // Render Power Crystal
+        DrawTextureRec(powerCrystal, powerCrystalRectangle, powerCrystalPosition, WHITE);
 
         // Ground Check
         if (kamiPosition.y >= windowHeight - kamiRectangle.height)
@@ -55,6 +72,18 @@ int main()
             velocity = 0;
             kamiPosition.y = windowHeight - kamiRectangle.height;
             inTheAir = false;
+
+            // Update Animation Frame
+            if (runningTime >= updateTime)
+            {
+                kamiRectangle.x = kamiRunningFrame * kamiRectangle.width;
+                kamiRunningFrame++;
+                if (kamiRunningFrame > 6)
+                {
+                    kamiRunningFrame = 0;
+                }
+                runningTime = 0;
+            }
         }
         else
         {
@@ -69,20 +98,11 @@ int main()
             velocity += jumpVelocity;
         }
         
-        // Update Position
+        // Update Kami's Position
         kamiPosition.y += velocity * deltaTime;
 
-        // Update Animation Frame
-        if (runningTime >= updateTime)
-        {
-            kamiRectangle.x = kamiRunningFrame * kamiRectangle.width;
-            kamiRunningFrame++;
-            if (kamiRunningFrame > 6)
-            {
-                kamiRunningFrame = 0;
-            }
-            runningTime = 0;
-        }
+        // Update Power Crystal's Position
+        powerCrystalPosition.x += powerCrystalVelocity * deltaTime;
 
         EndDrawing();
     }
